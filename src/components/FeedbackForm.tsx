f82106526f04cfa7571c1f888541b5948d7557f1
Badi1298 <serbandavid83@yahoo.com>
2024-05-01 00:17:47 +0300

    fetch feedbacks + types

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,42 @@
+import React, { useState } from 'react';
+
+import { MAX_CHARACTERS } from '../lib/constants';
+
 export default function FeedbackForm() {
+    const [feedback, setFeedback] = useState('');
+
+    const charCount = MAX_CHARACTERS - feedback.length;
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newText = e.target.value;
+        if (newText.length > MAX_CHARACTERS) return;
+
+        setFeedback(newText);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(feedback);
+    };
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <textarea
+                value={feedback}
                 name="feedback-textarea"
                 id="feedback-textarea"
                 placeholder="aaaa"
                 spellCheck="false"
+                onChange={handleChange}
             ></textarea>
             <label htmlFor="feedback-textarea">
                 Enter your feedback here, remember to #hashtag your company.
             </label>
 
             <div>
-                <p className="u-italic">150</p>
-                <button>
+                <p className="u-italic">{charCount}</p>
+                <button type="submit">
                     <span>Submit</span>
                 </button>
             </div>
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,26 +1,32 @@
-import { TriangleUpIcon } from '@radix-ui/react-icons';
+import { useEffect, useState } from 'react';
+
+import FeedbackItem from './FeedbackItem';
+
+type FeedbackItem = {
+    id: number;
+    upvoteCount: number;
+    badgeLetter: string;
+    companyName: string;
+    text: string;
+    daysAgo: number;
+};
 
 export default function FeedbackList() {
+    const [feedbackItems, setFeedbackItems] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+        )
+            .then(res => res.json())
+            .then(data => setFeedbackItems(data.feedbacks));
+    }, []);
+
     return (
         <ol className="feedback-list">
-            <li className="feedback">
-                <button>
-                    <TriangleUpIcon />
-                    <span>593</span>
-                </button>
-
-                <div>
-                    <p>B</p>
-                </div>
-
-                <div>
-                    <p>Serban</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Beatae hic alias, nostrum omnis nemo esse.
-                    </p>
-                </div>
-            </li>
+            {feedbackItems.map((item: FeedbackItem) => (
+                <FeedbackItem key={item.id} feedbackItem={item} />
+            ))}
         </ol>
     );
 }
